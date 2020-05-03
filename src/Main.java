d4af04bb830d72a7ecbdcaaef3bd59aa203eba80
Dina Katalsheva <katalsheva@yandex.ru>
2020-05-03 20:54:23 +0300

    commit innitial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]args) {
+        long ticketPrice = 12678;
+        int condition = 20;
+        long bonusMile = ticketPrice / condition;
+        System.out.println(bonusMile);
+    }
+}
